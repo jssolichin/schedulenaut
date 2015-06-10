4bd2ee3f8956f73d0ae2cce4ae0702408f4f418f
Jonathan Solichin <jssolichin@gmail.com>
2015-06-10 13:11:13 -0700

    Fix invitation emails.

--- source/js/event/controller.invitation.js
+++ source/js/event/controller.invitation.js
@@ -4,12 +4,41 @@
 
 'use strict';
 
-module.exports = function ($scope, mailServices) {
-	$scope.sendEmailToAll = function (event){
-		mailServices.sendMail({
-			to: 'jssolichin@gmail.com',
-			subject: 'The time for ' + event.name + ' has been set!',
-			html: event.description + ' <br> ' + event.location
-		});
+module.exports = function ($scope, mailServices, event, usersService) {
+
+    usersService.withEvent(event.id).then(function (users) {
+        $scope.users = users.data;
+    });
+
+	$scope.sendEmailToAll = function ($event){
+        var eventName = event.name || 'an event you are invited to';
+        
+        var eachMail = function (user){
+            var template = 
+            	"<img src='http://schedulenaut.com/public/images/logo.png'><h1>Schedulenaut</h1>" + 
+                "Hey " + user.name + ", <br>" + 
+                "<br>" + 
+                "Just wanted to let you know that the time and place for <b>" + eventName + "</b> has been set! Cool right? " + 
+                "<br>" + 
+                "<br>" + 
+                "Lift off to <a href=http://schedulenaut.com/event/" + event.id  + ">the event page</a> to know the details. <br>" + 
+                "<br>" + 
+                "Thanks!" + 
+                "<br>" + 
+                "Schedulenaut";
+
+            mailServices.sendMail({
+                to: user.email,
+                subject: 'The time for ' + eventName + ' has been set!',
+                html: template,
+            }).then(function (d){
+                $($event.target).html('Message Sent!');
+                $($event.target).attr('disabled', true);
+            });
+        };
+
+        $scope.users.forEach(function(user){
+            eachMail(user);
+        });
 	};
 };
